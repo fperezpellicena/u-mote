0f2d01a9642b9b91ada4e2163cfcc696da9ad541
fperezpellicena <fperezpellicena@gmail.com>
2011-11-03 16:42:52 +0000

    Métodos proxy para envío de tramas

--- src/digi/digi_proxy.c
+++ src/digi/digi_proxy.c
@@ -17,7 +17,56 @@
 
 #include "digi_proxy.h"
 
+static boolean XBeeProxy_sendPacket(XBeePacket* packet, uint8_t size) {
+	uint8_t* p = (uint8_t*) packet;
+	Serial_send(START_DELIMITER);
+	// send the most significant bit
+	Serial_send((size >> 8) & 0xFF);
+	// then the LSB
+	Serial_send(size & 0xFF); 
+	// just in case it hasn't been initialized.
+	packet->checksum = 0; 
+	//Generalizando para cualquier paquete
+	while (size--) {
+		Serial_send(*p);
+		packet->checksum += *p++;
+	}
+	Serial_send(0xFF - packet->checksum);
+	return true;
+}
 
-void XBeeProxy_sendAtCommand(AtCommand* const atCommand) {
-   
+boolean XBeeProxy_sendAtCommand(XBeePacket* packet, uint8_t length) {
+	int size = 0;
+	if(packet->apiId == AT_COMMAND || packet->apiId == AT_COMMAND_QUEUE) {
+		size = (length > 0) ? 8 : 4; // Asume que el comando se aplica
+		return XBeeProxy_sendPacket(packet, size);
+	}
+	return false;
+}
+
+boolean XBeeProxy_sendTransmitRequest(XBeePacket* packet, uint8_t length) {
+	int size = length;
+	if(packet->apiId == TRANSMIT_REQUEST) {
+		size += 14;
+		return XBeeProxy_sendPacket(packet, size);
+	}
+	return false;
+}
+
+boolean XBeeProxy_sendExplicitAddressing(XBeePacket* packet, uint8_t length) {
+	int size = length;
+	if(packet->apiId == EXPLICIT_ADDRESSING) {
+		size += 20;
+		return XBeeProxy_sendPacket(packet, size);
+	}
+	return false;
+}
+
+boolean XBeeProxy_sendRemoteAtCommand(XBeePacket* packet, uint8_t length) {
+	int size = length;
+	if(packet->apiId == REMOTE_AT_COMMAND) {
+		size += 16;
+		return XBeeProxy_sendPacket(packet, size);
+	}
+	return false;
 }
