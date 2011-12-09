a446dc4e7a9eb3a8bdf6578cce9efa0c37f01127
fperezpellicena@gmail.com <fperezpellicena@gmail.com@056e805e-48bb-9483-1167-f7677088c878>
2011-12-09 21:31:10 +0000

    Nombre de parámetro

--- trunk/test/debounce/debounce.X/nbproject/Makefile-default.mk
+++ trunk/test/debounce/debounce.X/nbproject/Makefile-default.mk
@@ -11,8 +11,11 @@
 include Makefile
 
 # Environment
+# Adding MPLAB X bin directory to path
+PATH:=/opt/microchip/mplabx/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
 MKDIR=mkdir -p
 RM=rm -f 
+MV=mv 
 CP=cp 
 
 # Macros
@@ -31,6 +34,9 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 # Distribution Directory
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1472/main.o
+
 # Object Files
 OBJECTFILES=${OBJECTDIR}/_ext/1472/main.o
 
@@ -40,7 +46,7 @@ ASFLAGS=
 LDLIBSOPTIONS=
 
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH=C:\\Archivos\ de\ programa\\Java\\jre6/bin/
+MP_JAVA_PATH="/usr/lib/jvm/java-6-sun-1.6.0.26/jre/bin/"
 OS_CURRENT="$(shell uname -s)"
 ############# Tool locations ##########################################
 # If you copy a project from one host to another, the path where the  #
@@ -48,50 +54,27 @@ OS_CURRENT="$(shell uname -s)"
 # If you open this project with MPLAB X in the new host, this         #
 # makefile will be regenerated and the paths will be corrected.       #
 #######################################################################
-MP_CC=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin\\mcc18.exe
+MP_CC="/opt/microchip/mplabc18/v3.40/bin/mcc18"
 # MP_BC is not defined
-MP_AS=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin\\..\\mpasm\\MPASMWIN.exe
-MP_LD=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin\\mplink.exe
-MP_AR=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin\\mplib.exe
-# MP_BC is not defined
-MP_CC_DIR=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin
+MP_AS="/opt/microchip/mplabc18/v3.40/bin/../mpasm/MPASMWIN"
+MP_LD="/opt/microchip/mplabc18/v3.40/bin/mplink"
+MP_AR="/opt/microchip/mplabc18/v3.40/bin/mplib"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar" 
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+MP_CC_DIR="/opt/microchip/mplabc18/v3.40/bin"
 # MP_BC_DIR is not defined
-MP_AS_DIR=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin\\..\\mpasm
-MP_LD_DIR=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin
-MP_AR_DIR=C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin
+MP_AS_DIR="/opt/microchip/mplabc18/v3.40/bin/../mpasm"
+MP_LD_DIR="/opt/microchip/mplabc18/v3.40/bin"
+MP_AR_DIR="/opt/microchip/mplabc18/v3.40/bin"
 # MP_BC_DIR is not defined
 
-# This makefile will use a C preprocessor to generate dependency files
-MP_CPP=C:/Archivos\ de\ programa/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/mplab-cpp
-
-.build-conf: ${BUILD_SUBPROJECTS}
+.build-conf:  ${BUILD_SUBPROJECTS}
 	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/debounce.X.${IMAGE_TYPE}.cof
 
 MP_PROCESSOR_OPTION=18F46J50
 MP_PROCESSOR_OPTION_LD=18f46j50
 MP_LINKER_DEBUG_OPTION=
-# ------------------------------------------------------------------------------------
-# Rules for buildStep: createRevGrep
-ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-__revgrep__:   nbproject/Makefile-${CND_CONF}.mk
-	@echo 'grep -q $$@' > __revgrep__
-	@echo 'if [ "$$?" -ne "0" ]; then' >> __revgrep__
-	@echo '  exit 0' >> __revgrep__
-	@echo 'else' >> __revgrep__
-	@echo '  exit 1' >> __revgrep__
-	@echo 'fi' >> __revgrep__
-	@chmod +x __revgrep__
-else
-__revgrep__:   nbproject/Makefile-${CND_CONF}.mk
-	@echo 'grep -q $$@' > __revgrep__
-	@echo 'if [ "$$?" -ne "0" ]; then' >> __revgrep__
-	@echo '  exit 0' >> __revgrep__
-	@echo 'else' >> __revgrep__
-	@echo '  exit 1' >> __revgrep__
-	@echo 'fi' >> __revgrep__
-	@chmod +x __revgrep__
-endif
-
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: assemble
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
@@ -102,42 +85,30 @@ endif
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${OBJECTDIR}/_ext/1472/main.o: ../main.c  nbproject/Makefile-${CND_CONF}.mk
-	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -p$(MP_PROCESSOR_OPTION)  --extended -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1472/main.o   ../main.c  > ${OBJECTDIR}/_ext/1472/main.err 2>&1 ; if [ $$? -eq 0 ] ; then cat ${OBJECTDIR}/_ext/1472/main.err | sed 's/\(^.*:.*:\)\(Warning\)\(.*$$\)/\1 \2:\3/g' ; else cat ${OBJECTDIR}/_ext/1472/main.err | sed 's/\(^.*:.*:\)\(Error\)\(.*$$\)/\1 \2:\3/g' ; exit 1 ; fi
-	${MP_CPP}  -MMD ${OBJECTDIR}/_ext/1472/main.o.temp ../main.c __temp_cpp_output__ -D __18F46J50 -D __18CXX -I C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin/../h  -D__18F46J50
-	printf "%s/" ${OBJECTDIR}/_ext/1472 > ${OBJECTDIR}/_ext/1472/main.o.d
-ifneq (,$(findstring MINGW32,$(OS_CURRENT)))
-	cat ${OBJECTDIR}/_ext/1472/main.o.temp | sed -e 's/\\\ /__SPACES__/g' -e's/\\$$/__EOL__/g' -e 's/\\/\//g' -e 's/__SPACES__/\\\ /g' -e 's/__EOL__/\\/g' >> ${OBJECTDIR}/_ext/1472/main.o.d
-else
-	cat ${OBJECTDIR}/_ext/1472/main.o.temp >> ${OBJECTDIR}/_ext/1472/main.o.d
-endif
-	${RM} __temp_cpp_output__
+	@${MKDIR} ${OBJECTDIR}/_ext/1472 
+	@${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -p$(MP_PROCESSOR_OPTION)  --extended -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1472/main.o   ../main.c 
+	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1472/main.o 
+	
 else
 ${OBJECTDIR}/_ext/1472/main.o: ../main.c  nbproject/Makefile-${CND_CONF}.mk
-	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
-	${MKDIR} ${OBJECTDIR}/_ext/1472 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION)  --extended -I ${MP_CC_DIR}\\..\\h  -fo ${OBJECTDIR}/_ext/1472/main.o   ../main.c  > ${OBJECTDIR}/_ext/1472/main.err 2>&1 ; if [ $$? -eq 0 ] ; then cat ${OBJECTDIR}/_ext/1472/main.err | sed 's/\(^.*:.*:\)\(Warning\)\(.*$$\)/\1 \2:\3/g' ; else cat ${OBJECTDIR}/_ext/1472/main.err | sed 's/\(^.*:.*:\)\(Error\)\(.*$$\)/\1 \2:\3/g' ; exit 1 ; fi
-	${MP_CPP}  -MMD ${OBJECTDIR}/_ext/1472/main.o.temp ../main.c __temp_cpp_output__ -D __18F46J50 -D __18CXX -I C:\\Archivos\ de\ programa\\Microchip\\mplabc18\\v3.40\\bin/../h  -D__18F46J50
-	printf "%s/" ${OBJECTDIR}/_ext/1472 > ${OBJECTDIR}/_ext/1472/main.o.d
-ifneq (,$(findstring MINGW32,$(OS_CURRENT)))
-	cat ${OBJECTDIR}/_ext/1472/main.o.temp | sed -e 's/\\\ /__SPACES__/g' -e's/\\$$/__EOL__/g' -e 's/\\/\//g' -e 's/__SPACES__/\\\ /g' -e 's/__EOL__/\\/g' >> ${OBJECTDIR}/_ext/1472/main.o.d
-else
-	cat ${OBJECTDIR}/_ext/1472/main.o.temp >> ${OBJECTDIR}/_ext/1472/main.o.d
-endif
-	${RM} __temp_cpp_output__
+	@${MKDIR} ${OBJECTDIR}/_ext/1472 
+	@${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION)  --extended -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1472/main.o   ../main.c 
+	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1472/main.o 
+	
 endif
 
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: link
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/debounce.X.${IMAGE_TYPE}.cof: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w -x -u_DEBUG  -u_EXTENDEDMODE -z__MPLAB_BUILD=1  -u_CRUNTIME -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PK3=1 $(MP_LINKER_DEBUG_OPTION) -l ${MP_CC_DIR}\\..\\lib  -odist/${CND_CONF}/${IMAGE_TYPE}/debounce.X.${IMAGE_TYPE}.cof ${OBJECTFILES}      
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w -x -u_DEBUG  -u_EXTENDEDMODE -z__MPLAB_BUILD=1  -u_CRUNTIME -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PK3=1 $(MP_LINKER_DEBUG_OPTION) -l ${MP_CC_DIR}/../lib  -odist/${CND_CONF}/${IMAGE_TYPE}/debounce.X.${IMAGE_TYPE}.cof  ${OBJECTFILES_QUOTED_IF_SPACED}   
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/debounce.X.${IMAGE_TYPE}.cof: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
-	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w   -u_EXTENDEDMODE -z__MPLAB_BUILD=1  -u_CRUNTIME -l ${MP_CC_DIR}\\..\\lib  -odist/${CND_CONF}/${IMAGE_TYPE}/debounce.X.${IMAGE_TYPE}.cof ${OBJECTFILES}      
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION_LD)  -w   -u_EXTENDEDMODE -z__MPLAB_BUILD=1  -u_CRUNTIME -l ${MP_CC_DIR}/../lib  -odist/${CND_CONF}/${IMAGE_TYPE}/debounce.X.${IMAGE_TYPE}.cof  ${OBJECTFILES_QUOTED_IF_SPACED}   
 endif
 
 
@@ -152,4 +123,7 @@ endif
 # Enable dependency checking
 .dep.inc: .depcheck-impl
 
-include .dep.inc
+DEPFILES=$(wildcard $(addsuffix .d, ${OBJECTFILES}))
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
--- trunk/test/debounce/main.c
+++ trunk/test/debounce/main.c
@@ -18,88 +18,88 @@
 #include <p18f46j50.h>
 
 #pragma config WDTEN = OFF          /* Disabled - Controlled by SWDTEN bit                                                       */
-   
+
 void debounceSwitch(void);
 int debounce(int bit);
 
 #pragma interruptlow switchChange
 
 void switchChange() {
-	char reg;
-	if (INTCONbits.RBIF) {
-		reg = PORTB;
-		_asm nop _endasm
-		INTCONbits.RBIF = 0;
-		// Configure Timer0 OVF int.
-		INTCONbits.TMR0IE = 1;
-		// Start periodic sampling
-		T0CONbits.TMR0ON = 1;
-	}
+    char reg;
+    if (INTCONbits.RBIF) {
+        reg = PORTB;
+        _asm nop _endasm
+                INTCONbits.RBIF = 0;
+        // Configure Timer0 OVF int.
+        INTCONbits.TMR0IE = 1;
+        // Start periodic sampling
+        T0CONbits.TMR0ON = 1;
+    }
 }
 
 #pragma interrupt debounceSwitch
 
 void debounceSwitch(void) {
-	 if (INTCONbits.TMR0IF) {
-		INTCONbits.TMR0IF = 0;
-		if (debounce(LATBbits.LATB4)) {
-			// Stop timer
-			T0CONbits.TMR0ON = 0;
-			TMR0H = 0; //clear timer
-			TMR0L = 0; //clear timer 
-			// Toggle
-			LATEbits.LATE0 = ~LATEbits.LATE0;
-		}
-	}
+    if (INTCONbits.TMR0IF) {
+        INTCONbits.TMR0IF = 0;
+        if (debounce(LATBbits.LATB4)) {
+            // Stop timer
+            T0CONbits.TMR0ON = 0;
+            TMR0H = 0; //clear timer
+            TMR0L = 0; //clear timer
+            // Toggle
+            LATEbits.LATE0 = ~LATEbits.LATE0;
+        }
+    }
 }
 
 #pragma code HIGH_INTERRUPT_VECTOR = 0x08
 
 void high_ISR(void) {
-	_asm goto debounceSwitch _endasm
+    _asm goto debounceSwitch _endasm
 }
 #pragma code
 
 #pragma code LOW_INTERRUPT_VECTOR = 0x18
 
 void low_ISR(void) {
-	_asm goto switchChange _endasm
+    _asm goto switchChange _endasm
 }
 #pragma code
 
-int debounce(int bit) {
-	static unsigned long state = 0;
-	state = state << 1 | !bit | 0xE000;
-	if (state > 0xF000) {
-		return 1;
-	}
-	return 0;
+int debounce(int portBit) {
+    static unsigned long state = 0;
+    state = state << 1 | !portBit | 0xE000;
+    if (state > 0xF000) {
+        return 1;
+    }
+    return 0;
 }
 
 void main(void) {
-	TRISEbits.TRISE0 = 0;
-	LATEbits.LATE0 = 0;
-	TRISBbits.TRISB4 = 1;
+    TRISEbits.TRISE0 = 0;
+    LATEbits.LATE0 = 0;
+    TRISBbits.TRISB4 = 1;
 
-	// Configure Timer0
-	T0CONbits.T0PS = 7;
-	T0CONbits.T08BIT = 1;
-	T0CONbits.T0CS = 0;
-	TMR0H = 0; //clear timer
-	TMR0L = 0; //clear timer
+    // Configure Timer0
+    T0CONbits.T0PS = 7;
+    T0CONbits.T08BIT = 1;
+    T0CONbits.T0CS = 0;
+    TMR0H = 0; //clear timer
+    TMR0L = 0; //clear timer
 
 
-	INTCONbits.RBIE = 1;
-	INTCON2bits.RBIP = 0;
-	INTCON2bits.TMR0IP = 1;
+    INTCONbits.RBIE = 1;
+    INTCON2bits.RBIP = 0;
+    INTCON2bits.TMR0IP = 1;
 
-	RCONbits.IPEN = 1;
-	INTCONbits.GIEH = 1;
-	INTCONbits.GIEL = 1;
+    RCONbits.IPEN = 1;
+    INTCONbits.GIEH = 1;
+    INTCONbits.GIEL = 1;
 
-	T0CONbits.TMR0ON = 0;
-	INTCONbits.RBIF = 0;
-	INTCONbits.TMR0IF = 0;
+    T0CONbits.TMR0ON = 0;
+    INTCONbits.RBIF = 0;
+    INTCONbits.TMR0IF = 0;
 
-	while (1);
+    while (1);
 }
