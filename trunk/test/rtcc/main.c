d8b514cd8891220ec2c1a0e2f0059144f3e58ac9
fperezpellicena@gmail.com <fperezpellicena@gmail.com@056e805e-48bb-9483-1167-f7677088c878>
2012-02-19 17:34:03 +0000

    Tests

--- trunk/test/rtcc/main.c
+++ trunk/test/rtcc/main.c
@@ -15,146 +15,133 @@
  *  along with uMote.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// RTCC test
-
-#include <stdlib.h>
-#include <rtcc.h>
+#include <p18cxxx.h>
+#include <stdio.h>
 #include <usart.h>
+#include <timers.h>
+#include <rtcc.h>
 
-rtccDate date;
-rtccTime time;
-rtccTimeDate dateTime;
+/* Pragmas */
+#pragma config WDTEN = OFF          //WDT disabled (enabled by SWDTEN bit)
+//#pragma config PLLDIV = 3           //Divide by 3 (12 MHz oscillator input)
+#pragma config STVREN = ON          //stack overflow/underflow reset enabled
+#pragma config XINST = OFF           //Extended instruction set disabled
+#pragma config CPUDIV = OSC1        //No CPU system clock divide
+#pragma config CP0 = OFF            //Program memory is not code-protected
+//#pragma config OSC = HSPLL          //HS oscillator, PLL enabled, HSPLL used by USB
 
-rtccDate alrmDate;
-rtccTime alrmTime;
-rtccTimeDate alrmDateTime;
+#pragma config OSC = INTOSCPLL      // Internal OSC(8mhz), PLL enabled by soft
+#pragma config PLLDIV = 2           // Divide By 2(4 Mhz) to generate 96Mhz
 
-const far rom char eol[] = "\r\n";
+#pragma config FCMEN = OFF          //Fail-Safe Clock Monitor disabled
+#pragma config IESO = OFF           //Two-Speed Start-up disabled
+#pragma config WDTPS = 32768        //1:32768
+#pragma config DSWDTOSC = INTOSCREF //DSWDT uses INTOSC/INTRC as clock
+#pragma config RTCOSC = INTOSCREF   //RTCC uses INTRC
+#pragma config DSBOREN = OFF        //Zero-Power BOR disabled in Deep Sleep
+#pragma config DSWDTEN = OFF        //Disabled
+#pragma config DSWDTPS = 8192       //1:8,192 (8.5 seconds)
+#pragma config IOL1WAY = OFF        //IOLOCK bit can be set and cleared
+#pragma config MSSP7B_EN = MSK7     //7 Bit address masking
+#pragma config WPFP = PAGE_1        //Write Protect Program Flash Page 0
+#pragma config WPEND = PAGE_0       //Start protection at page 0
+#pragma config WPCFG = OFF          //Write/Erase last page protect Disabled
+#pragma config WPDIS = OFF          //WPFP[5:0], WPEND, and WPCFG bits ignored  
 
-const far rom char start[] = "Start test\r\n";
-const far rom char end[] = "End test\r\n";
-const far rom char dateSet[] = "Date set\r\n";
-const far rom char dateRead[] = "Date read\r\n";
-const far rom char timeSet[] = "Time set\r\n";
-const far rom char timeRead[] = "Time read\r\n";
+// BAUDRG is calculated as  = Fosc / (4 * Desired Baudrate) - 1
+// So, 4MHz / (4 * 19200) - 1 = 52 (approx.)
+//#define BAUDRG 51                   // 19.2Kbps from 4MHz INTOSC
+//#define BAUDRG 103                  // 115.2Kbps from 48MHz
+//#define BAUDRG 89                   // 115.2Kbps from 41.66MHz
+//#define BAUDRG 85                   // 115.2Kbps from 40MHz
+//#define BAUDRG 68                   // 115.2Kbps from 32MHz
+#define BAUDRG 16                   // 115.2Kbps from 8MHz
+//#define BAUDRG 11                   // 1Mbps from 48MHz
+//#define BAUDRG 9                    // 1Mbps from 40MHz
+//#define BAUDRG 4                    // 2Mbps from 40MHz
+//#define BAUDRG 3                    // 3Mbps from 48MHz
+//#define BAUDRG 1                    // 6Mbps from 48MHz
+#ifndef BAUDRG
+#define BAUDRG brg              // use bootloader autobaud BRG if constant not provided
+#endif
+//#define INVERT_UART                 // If you don't have an RS232 transceiver, you might want this option
 
-const far rom char wday[] = "wday:";
-const far rom char mon[] = "mon:";
-const far rom char mday[] = "mday:";
-const far rom char year[] = "year:";
+void PrintTime(void);
+void high_isr(void);
+void low_isr(void);
 
-const far rom char hour[] = "hour:";
-const far rom char min[] = "min:";
-const far rom char sec[] = "sec:";
+rtccTimeDate d;
 
-char str[2];
+unsigned int brg;
 
 void main() {
-	Open1USART(USART_TX_INT_OFF	& USART_RX_INT_ON & USART_ASYNCH_MODE &
-			USART_EIGHT_BIT & USART_CONT_RX & USART_BRGH_HIGH, 25);
-	putrs1USART(start);
-	
-	// Init clk
-	RtccInitClock();
+    OSCTUNEbits.PLLEN = 0; // disable PLL
+    OSCCON = 0b01110011; // 8MHz INTOSC
 
-	// Set date
-	date.f.wday = 6;	// Domingo
-	date.f.mon = 12;	// Diciembre
-	date.f.mday = 4;	// Día del mes: 4
-	date.f.year = 11;	// Año 2011
-	RtccWriteDate(&date, TRUE);
+#ifdef INVERT_UART
+    LATCbits.LATC6 = 0; // drive TXD pin low for RS-232 IDLE state
+#else
+    LATCbits.LATC6 = 1; // drive TXD pin high for RS-232 IDLE state
+#endif
+    TRISCbits.TRISC6 = 0;
 
-	// Set time
-	time.f.hour = 12;	// Hora del día
-	time.f.min = 0;		// Minuto
-	time.f.sec = 0;		// Segundo
-	RtccWriteTime(&time, TRUE);
+    // Enable the Secondary Oscillator for RTCC use.
+    //OpenTimer1(TIMER_INT_OFF & T1_SOURCE_PINOSC & T1_PS_1_1 & T1_OSC1EN_ON & T1_SYNC_EXT_OFF, 0);
+    //T1CONbits.T1OSCEN = 1;
 
-	// Set datetime
-	dateTime.f.wday = 6;	// Domingo
-	dateTime.f.mon = 12;	// Diciembre
-	dateTime.f.mday = 4;	// Día del mes: 4
-	dateTime.f.year = 11;	// Año 2011
-	dateTime.f.hour = 12;	// Hora del día
-	dateTime.f.min = 0;		// Minuto
-	dateTime.f.sec = 0;		// Segundo
-	RtccWriteTimeDate(&dateTime, TRUE);
+    // Did we wake up from Deep Sleep? Or is this the first initial power on?
+    if (WDTCONbits.DS) {
+        // woke up from Deep Sleep
+        DSCONLbits.RELEASE = 0; // release control and data bits for all I/Os
+    } else if (RCONbits.POR == 0) {
+        RCONbits.POR = 1;
+//        // first initial power up of the device.
+//        // unlock the RTCC registers so that we can write to them
+        EECON2 = 0x55;
+        EECON2 = 0xAA;
+        RTCCFGbits.RTCWREN = 1;
 
-	putrs1USART(dateSet);
-	putrs1USART(wday);
-	puts1USART(itoa(date.f.wday, str));
-	putrs1USART(eol);
-	putrs1USART(mon);
-	puts1USART(itoa(date.f.mon, str));
-	putrs1USART(eol);
-	putrs1USART(mday);
-	puts1USART(itoa(date.f.mday, str));
-	putrs1USART(eol);
-	putrs1USART(year);
-	puts1USART(itoa(date.f.year, str));
-	putrs1USART(eol);
+        
+        d.f.hour = 0x0A;
+        d.f.min = 9;
+        d.f.sec = 0x1E;
+        d.f.year = 12;
+        d.f.mon = 2;
+        d.f.mday = 18;
+        d.f.wday = 6;
 
-	// Read date
-	RtccReadDate(&date);
-	putrs1USART(dateRead);
-	putrs1USART(wday);
-	puts1USART(itoa(date.f.wday, str));
-	putrs1USART(eol);
-	putrs1USART(mon);
-	puts1USART(itoa(date.f.mon, str));
-	putrs1USART(eol);
-	putrs1USART(mday);
-	puts1USART(itoa(date.f.mday, str));
-	putrs1USART(eol);
-	putrs1USART(year);
-	puts1USART(itoa(date.f.year, str));
-	putrs1USART(eol);
+        RtccWriteTimeDate(&d, TRUE);
 
-	putrs1USART(timeSet);
-	putrs1USART(hour);
-	puts1USART(itoa(time.f.hour, str));
-	putrs1USART(eol);
-	putrs1USART(min);
-	puts1USART(itoa(time.f.min, str));
-	putrs1USART(eol);
-	putrs1USART(sec);
-	puts1USART(itoa(time.f.sec, str));
-	putrs1USART(eol);
+        RTCCFGbits.RTCEN = 1; // enable RTCC module
 
-	// Read time
-	RtccReadTime(&time);
-	putrs1USART(timeRead);
-	putrs1USART(hour);
-	puts1USART(itoa(time.f.hour, str));
-	putrs1USART(eol);
-	putrs1USART(min);
-	puts1USART(itoa(time.f.min, str));
-	putrs1USART(eol);
-	putrs1USART(sec);
-	puts1USART(itoa(time.f.sec, str));
-	putrs1USART(eol);
+        //RTCCFGbits.RTCOE = 1;
+    }
+    // PADCFG1 = 0b100; // enable source clock as output on RTCC pin (RB1)
 
-	// Read datetime
-	RtccReadTimeDate(&dateTime);
+    IPR1bits.RC1IP = 0; // USART RX interrupt as low priority interrupt
+    RCONbits.IPEN = 1; // enable interrupt priority
+    INTCONbits.GIEH = 1;
+    INTCONbits.GIEL = 1;
 
-	// Set alarm date
-	RtccWriteAlrmDate(&alrmDate);
+    Open1USART(USART_TX_INT_OFF & USART_RX_INT_ON & USART_EIGHT_BIT & USART_ASYNCH_MODE & USART_ADDEN_OFF, BAUDRG);
+#ifdef INVERT_UART
+    baud1USART(BAUD_IDLE_TX_PIN_STATE_LOW & BAUD_IDLE_RX_PIN_STATE_LOW & BAUD_AUTO_OFF & BAUD_WAKEUP_OFF & BAUD_16_BIT_RATE);
+#else
+    baud1USART(BAUD_IDLE_TX_PIN_STATE_HIGH & BAUD_IDLE_RX_PIN_STATE_HIGH & BAUD_AUTO_OFF & BAUD_WAKEUP_OFF & BAUD_16_BIT_RATE);
+#endif
 
-	// Set alarm time
-	RtccWriteAlrmTime(&alrmTime);
+    printf((far rom char*) "\r\nConnect scope to RB1 for 32.768KHz RTC clock output signal.\r\n");
 
-	// Set alarm datetime
-	RtccWriteAlrmTimeDate(&alrmDateTime);
+    // infinite loop, interrupts will handle incoming UART communications.
+    while (1) {
+        PrintTime();
+    }
+}
 
-	// Read alarm date
-	RtccReadAlrmDate(&alrmDate);
-
-	// Read alarm time
-	RtccReadAlrmTime(&alrmTime);
-
-	// Read alarm datetime
-	RtccReadAlrmTimeDate(&alrmDateTime);
-
-	puts1USART(end);
-	while(1);
+void PrintTime(void) {
+    // unsigned char year, yearh, month, day, hour, min, sec;
+    while (RTCCFGbits.RTCSYNC != 1); // if already in the middle of SYNC, wait for next period
+    while (RTCCFGbits.RTCSYNC == 1); // wait while RTCC registers are unsafe to read
+    RtccReadTimeDate(&d);
+    printf("\r20%02d/%02d/%02d %02X:%02X:%02X", d.f.year, d.f.mon, d.f.mday, d.f.hour, d.f.min, d.f.sec);
 }
--- trunk/test/sht.X/main.c
+++ trunk/test/sht.X/main.c
@@ -1,3 +1,19 @@
+/**
+ * This file is part of uMote.
+ *
+ *  uMote is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *  uMote is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with uMote.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <p18f46j50.h>
 #include <usart.h>
 #include <stdio.h>
@@ -56,7 +72,7 @@ static char write(unsigned char value) {
 
 //----------------------------------------------------------------------------------
 
-unsigned char read(unsigned char ack)
+static unsigned char read(unsigned char ack)
 //----------------------------------------------------------------------------------
 // reads a byte form the Sensibus and gives an acknowledge in case of "ack=1"
 {
